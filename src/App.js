175e7f55edd18ba3eeccc000c0e83368a21e45c2
Jose <ottonellojose@hotmail.com>
2022-06-15 19:57:53 -0300

    first save

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <ItemListContainer/>
+    </div>
+  );
+}
+
+export default App;
